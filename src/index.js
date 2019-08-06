6080a3f2c78ad116ede1110ed15fad610c4efcb1
atckyl <ackylmohamad@gmail.com>
2019-08-06 12:40:31 +0700

    first push

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const powrt = require('./config/port')
+
+const userRouter = require('./routers/userRouter')
+const taskRouter = require('./routers/taskRouter')
+
+const app = express()
+const port = powrt
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome!</h1>')
+})
+
+app.listen(port, () => {
+    console.log('Berhasil Running di ' + port);
+    
+})
